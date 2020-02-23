ab7e6f4aac1bcee513c9d8c172b3c65282f2ebe1
AldenBoby <boby.alden128@gmail.com>
2020-02-23 18:36:28 +0200

    Task 1 Q 12 added cs,csproj and sln files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        static void Prefix(string input)
+        {
+            int num_letters = input.Length;
+
+            int loop = 0;
+            int word_counter = 0;
+            bool space = true;
+
+            while (loop<num_letters)
+            { 
+                while (input[loop]==' ')
+                {
+                    space = true;
+                    loop++;
+                    if (loop==num_letters)
+                    {
+                        space = false;
+                        break;
+                    }
+                }
+                if (space == true)
+                {
+                    word_counter++;
+                    space = false;
+                }
+                loop++;
+            }
+            Console.WriteLine(num_letters+","+word_counter+":"+input);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Prefix("Hello");
+            Prefix("");
+            Prefix("what    ...  did you say?? ");
         }
     }
 }

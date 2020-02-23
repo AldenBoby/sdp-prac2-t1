73c7f4f29142452464a57e2e21f34b023b28eaf9
AldenBoby <boby.alden128@gmail.com>
2020-02-23 21:44:52 +0200

    Task 1 Q16

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Problems;
 
 namespace Tests
 {
@@ -12,7 +13,23 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Program P = new Program();
+            string Test1 = P.Prefix("hello");
+            Assert.That(Test1, Is.EqualTo("5,1:hello"));
+        }
+        [Test]
+        public void Test2()
+        {
+            Program P = new Program();
+            string Test2 = P.Prefix("");
+            Assert.That(Test2, Is.EqualTo("0,0:"));
+        }
+        [Test]
+        public void Test3()
+        {
+            Program P = new Program();
+            string Test3 = P.Prefix("what    ...  did you say?? ");
+            Assert.That(Test3, Is.EqualTo("27,5:what    ...  did you say?? "));
         }
     }
 }
--- Problems/Program.cs
+++ Problems/Program.cs
@@ -4,40 +4,40 @@ namespace Problems
 {
     public class Program
     {
-        static void Prefix(string input)
+        public string Prefix(string input)
         {
             int num_letters = input.Length;
-
             int loop = 0;
             int word_counter = 0;
-            bool space = true;
+            bool space = true;//initialised at true so first word is counted
 
             while (loop<num_letters)
             { 
                 while (input[loop]==' ')
                 {
                     space = true;
-                    loop++;
+                    loop++;//iterate through space chars
                     if (loop==num_letters)
                     {
-                        space = false;
-                        break;
+                        space = false;//prevent counter from increasing when result is found
+                        break;//prevent out of bounds
                     }
                 }
                 if (space == true)
                 {
-                    word_counter++;
+                    word_counter++;//starting new word 
                     space = false;
                 }
                 loop++;
             }
-            Console.WriteLine(num_letters+","+word_counter+":"+input);
+            return num_letters+","+word_counter+":"+input;//return output string
         }
         static void Main(string[] args)
         {
-            Prefix("Hello");
-            Prefix("");
-            Prefix("what    ...  did you say?? ");
+            Program Prfx = new Program();
+            Prfx.Prefix("Hello");
+            Prfx.Prefix("");
+            Prfx.Prefix("what    ...  did you say?? ");
         }
     }
 }
